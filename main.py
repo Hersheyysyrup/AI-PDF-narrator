5ec85bb863b4bdc67e51b1ed9db1a8648ba7e0dd
Harshit Nayan <harshitrajeevnayan@gmail.com>
2026-08-16 23:52:47 +0530

    vector db

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from document_loaders.pdf import load_pdf
 from text_splitters.splitters import split_documents
 from embeddings.embeddings import get_embedding_model
+from vector_store.chroma import create_vector_store
 
-pdf_path = "data/rag.pdf"
+pdf_path = "AI PDF narrator/data/rag.pdf"
 
 documents = load_pdf(pdf_path)
 print(f"Loaded {len(documents)} pages")
@@ -12,3 +13,6 @@ print(f"Created {len(chunks)}chunks")
 
 embedding_model = get_embedding_model()
 print("Embedding Model Loaded")
+
+vector_store = create_vector_store(chunks, embedding_model)
+print("Vector store created")
