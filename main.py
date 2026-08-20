bb8a4a4fb8f94b0b6d02e9adab06b0d03a48b800
Harshit Nayan <harshitrajeevnayan@gmail.com>
2026-08-20 20:56:26 +0530

    User Input Test

--- main.py
+++ main.py
@@ -1,44 +1,63 @@
+import os
+import shutil
 from document_loaders.pdf import load_pdf
 from text_splitters.splitters import split_documents
 from embeddings.embeddings import get_embedding_model
-from vector_store.chroma import create_vector_store
+from vector_store.chroma import ( create_vector_store)
 from retriever.retriever import get_mmr_retriever
 from gen_ai.gen import generate_answer
 
-pdf_path = "AI PDF narrator/data/rag.pdf"
+CHROMA_DB_DIR = "chroma_db"
 
-documents = load_pdf(pdf_path)
-print(f"Loaded {len(documents)} pages")
+def ingest_pdf(pdf_path):
 
-chunks = split_documents(documents)
-print(f"Created {len(chunks)}chunks")
+    if not os.path.exists(pdf_path):
+        raise FileNotFoundError(f"PDF file not found: {pdf_path}")
 
-embedding_model = get_embedding_model()
-print("Embedding Model Loaded")
+    if os.path.exists(CHROMA_DB_DIR):
+        shutil.rmtree(CHROMA_DB_DIR)
 
-vector_store = create_vector_store(chunks, embedding_model)
-print("Vector store created")
+    documents = load_pdf(pdf_path)
+    print(f"Loaded {len(documents)} pages")
 
-def ask(question,retriever):
+    chunks = split_documents(documents)
+    print(f"Split into {len(chunks)} chunks")
+
+    embedding_model = get_embedding_model()
+
+    vector_store = create_vector_store(chunks, embedding_model)
+    print(f"Vector store created for the current PDF\n")
+    return vector_store
+
+def ask(question , retriever):
+
     docs = retriever.invoke(question)
-    context = "\n\n".join(
-        doc.page_content for doc in docs
-        )
-    answer = generate_answer(context,question)
-    return answer
+
+    context = "\n".join([doc.page_content for doc in docs])
+
+    return generate_answer(context, question)
 
 def main():
-    retriever = get_mmr_retriever(vector_store)
 
-    print("Ask questions about your PDF. Type'exit' to quit.\n")
+    pdf_path = input("Enter the path for your pdf: ").strip().strip('"')
+
+    vector_store = ingest_pdf(pdf_path)
+
+    retriever = get_mmr_retriever(vector_store)
+    print("Ask Questions from the PDF")
+    print("Type 'exit' to quit the program\n")
+
     while True:
-        question = input("Ask a Question: ")
-        if question.lower() in ("exit","quit", "q"):
-            print("Goodbye!")
+
+        question = input("Ask a question: ")
+
+        if question.lower() in ["exit", "quit", "q"]:
+            print("Sayonara!")
             break
 
         answer = ask(question, retriever)
-        print(f"\nAnswer : {answer}\n")
+
+        print(f"\nAnswer: {answer}\n")
 
 if __name__ == "__main__":
     main()
